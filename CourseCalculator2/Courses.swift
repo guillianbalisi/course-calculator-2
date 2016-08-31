23166d5112dd225f445dead41a59cb3681a5bf56
guillianbalisi <guillianbalisi@gmail.com>
2016-08-31 16:58:35 -0400

    Created model files

---
+++ CourseCalculator2/Courses.swift
@@ -0,0 +1,28 @@
+//
+//  Courses.swift
+//  CourseCalculator2
+//
+//  Created by Guillian Balisi on 2016-08-31.
+//  Copyright © 2016 Guillian Balisi. All rights reserved.
+//
+
+import Foundation
+
+class Courses {
+    
+    var courseList: [String]
+    
+    init() {
+        self.courseList = []
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        self.courseList = aDecoder.decodeObjectForKey("courseList") as! [String]
+    }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(courseList, forKey: "courseList")
+    }
+    
+    
+}
---
+++ CourseCalculator2/Grades.swift
@@ -0,0 +1,74 @@
+//
+//  Grades.swift
+//  CourseCalculator2
+//
+//  Created by Guillian Balisi on 2016-08-31.
+//  Copyright © 2016 Guillian Balisi. All rights reserved.
+//
+
+import Foundation
+
+class Grades {
+    var gradeList: [Double]
+    var weightList: [Double]
+    
+    init() {
+        gradeList = []
+        weightList = []
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        self.gradeList = aDecoder.decodeObjectForKey("gradeList") as! [Double]
+        self.weightList = aDecoder.decodeObjectForKey("weightList") as! [Double]
+    }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(gradeList, forKey: "gradeList")
+        aCoder.encodeObject(weightList, forKey: "weightList")
+    }
+    
+    func sumGrade() -> Double {
+        let sum = gradeList.reduce(0, combine: +)
+        return sum
+    }
+    
+    func sumWeight() -> Double {
+        let sum = weightList.reduce(0, combine: +)
+        return sum
+    }
+    
+    func add(grade: Double, weight: Double) {
+        gradeList.append(grade)
+        weightList.append(weight)
+    }
+    
+    func currentAverage(gradeList: [Double], weightList: [Double]) -> Double {
+        var currentGrade: Double = 0
+        for i in 0..<gradeList.count {
+            currentGrade += gradeList[i] * weightList[i] * 0.01
+        }
+        return currentGrade
+    }
+    
+    func currentWeight() -> Double {
+        return sumWeight()
+    }
+    
+    func remainingWeight() -> Double {
+        return 100 - sumWeight()
+    }
+    
+    func requiredAverage(inputFinalGrade: Double) -> Double { // required average based on final grade
+        let currentGrade = currentAverage(gradeList, weightList: weightList)
+        return (inputFinalGrade - currentGrade / remainingWeight())
+    }
+    
+    func overallAverage(inputRemainingGrade: Double) -> Double { // overall average based on remaining grade
+        var tempGradeList = gradeList
+        var tempWeightList = weightList
+        tempGradeList.append(inputRemainingGrade)
+        tempWeightList.append(remainingWeight())
+        return currentAverage(tempGradeList, weightList: tempWeightList)
+    }
+    
+}
