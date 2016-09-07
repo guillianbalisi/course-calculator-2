e452f61171940e6476e5d2c6eb82554b872ac1b2
guillianbalisi <guillianbalisi@gmail.com>
2016-09-07 10:52:59 -0400

    SCLAlertView added

---
+++ CourseCalculator2/ThirdViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ThirdViewController.swift
+//  CourseCalculator2
+//
+//  Created by Guillian Balisi on 2016-09-07.
+//  Copyright © 2016 Guillian Balisi. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+        
+}
